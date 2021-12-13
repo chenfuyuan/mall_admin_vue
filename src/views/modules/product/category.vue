959412c91e79bcb5a149b43fe4b005ef34f7f031
chenfuyuan <chenfuyuan0713@163.com>
2021-12-13 19:22:01 +0800

    feat(product): 实现分类的修改
    1. 实现分类的修改

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -8,6 +8,8 @@
       :expand-on-click-node="false"
       node-key="catId"
       :default-expanded-keys="expandedKeys"
+      @node-expand="expandHandle"
+      @node-collapse="collapseHandle"
     >
       <!--菜单操作按钮组-->
       <span class="custom-tree-node" slot-scope="{ node, data }">
@@ -22,6 +24,10 @@
           >
             添加
           </el-button>
+          <!--更新按钮-->
+          <el-button type="text" size="mini" @click="update(data)">
+            更改
+          </el-button>
           <!--刪除按钮-->
           <el-button
             v-if="node.childNodes.length == 0"
@@ -35,20 +41,37 @@
       </span>
     </el-tree>
 
-    <!--添加弹出框-->
-
     <!--添加分类弹出框-->
     <div class="add-dialog">
-      <el-dialog title="添加分类" :visible.sync="addDialogFormVisible" width="30%">
+      <el-dialog
+        :title="dialogConfig.title"
+        :visible.sync="dialogFormVisible"
+        width="30%"
+      >
         <el-form :model="category">
+          <!--分类名称-->
           <el-form-item label="分类名称">
             <el-input v-model="category.name" autocomplete="off"></el-input>
           </el-form-item>
+
+          <!--图标-->
+          <el-form-item label="图标">
+            <el-input v-model="category.icon" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <!--计量单位-->
+          <el-form-item label="计量单位">
+            <el-input
+              v-model="category.productUnit"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
         </el-form>
+
         <div slot="footer" class="dialog-footer">
-          <el-button type="primary" @click="() => addCategory()"
-            >添加</el-button
-          >
+          <el-button type="primary" @click="submit()">
+            {{dialogConfig.buttonText}}
+          </el-button>
         </div>
       </el-dialog>
     </div>
@@ -60,15 +83,41 @@ import Vue from "vue";
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 
+//新的分类
+const NEW_CATEGORY = {
+  catId: null,
+  name: "",
+  parentCid: "",
+  catLevel: "",
+  showStatus: 1,
+  sort: 0,
+  productCount: 0,
+  icon: "",
+  productUnit: "",
+};
+
+const DIALOG_CONFIG = {
+  add: {
+    type: "add",
+    title: "添加分类",
+    buttonText: "添加",
+  },
+  update: {
+    type: "update",
+    title: "修改分类",
+    buttonText: "修改",
+  },
+};
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
     //这里存放数据
     return {
-      currentCategory:{},
+      dialogConfig: {},
+      currentCategory: {},
       expandedKeys: [],
-      addDialogFormVisible: false, //添加分类弹出框显示状态
+      dialogFormVisible: false, //添加分类弹出框显示状态
       category: {}, //弹出框表单信息
       menus: [], //菜单
       defaultProps: {
@@ -83,6 +132,21 @@ export default {
   watch: {},
   //方法集合
   methods: {
+
+    /**
+     * 节点收缩
+     */
+    collapseHandle(data,node,component){
+      let index = this.expandedKeys.findIndex((item)=>item===data.catId);
+      this.expandedKeys.splice(index,1);
+    },
+
+    /**
+     * 节点展开
+     */
+    expandHandle(data,node,component){
+      this.expandedKeys.push(data.catId);
+    },
     //获取菜单
     getMenus() {
       this.$http({
@@ -91,6 +155,67 @@ export default {
       }).then(({ data }) => {
         this.menus = data.data;
       });
+    },
+
+    /**
+     * 提交
+     */
+    submit() {
+      if (this.dialogConfig.type == "add") {
+        this.addCategory();
+      }
+      if (this.dialogConfig.type == "update") {
+        this.updateCategory();
+      }
+    },
+
+    /**
+     * 更新分类
+     */
+    updateCategory() {
+      let { catId, name, icon, productUnit } = this.category;
+      //发送更新请求
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false),
+      }).then(({ data }) => {
+        this.$message({
+          showClose: true,
+          message: "更新成功",
+          type: "success",
+        });
+
+        //关闭弹窗
+        this.dialogFormVisible = false;
+        
+        //刷新菜单
+        this.getMenus();
+        this.expandedKeys.push(this.category.catId);
+      });
+    },
+
+    /**
+     * 更新按钮
+     */
+    update(data) {
+      //从后台获取
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      })
+        .then(({ data }) => {
+          this.category = data.category;
+          this.dialogFormVisible = true;
+          this.dialogConfig = DIALOG_CONFIG.update;
+        })
+        .catch(() => {
+          this.$message({
+            showClose: true,
+            message: "更新失败",
+            type: "error",
+          });
+        });
     },
 
     /**
@@ -108,14 +233,13 @@ export default {
           message: "添加成功",
           type: "success",
         });
-        debugger;
-        //添加到当前菜单
-        this.category.catId = data.saveCatId;
-        //添加进数组的是引用地址，应该将该对象复制一份放入数组
-        this.currentCategory.subCategorys.push({...this.category});
+
+        //刷新菜单
+        this.getMenus();
         this.expandedKeys.push(data.saveCatId);
+
         //关闭弹窗
-        this.addDialogFormVisible = false;
+        this.dialogFormVisible = false;
       });
     },
 
@@ -124,18 +248,17 @@ export default {
      */
     append(data) {
       //初始化category对象
-      this.category = {
-        catId: "",
-        name: "",
-        parentCid: data.catId,
-        catLevel: data.catLevel * 1 + 1,
-        showStatus: 1,
-        sort: 0,
-        productCount: 0,
-      };
-      this.currentCategory=data;
+      this.category = { ...NEW_CATEGORY };
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel + 1;
+
+      //记录当前操作分类
+      this.currentCategory = data;
+
       //显示弹框
-      this.addDialogFormVisible = true;
+      this.dialogFormVisible = true;
+      debugger;
+      this.dialogConfig = DIALOG_CONFIG.add;
     },
 
     /**
@@ -164,11 +287,12 @@ export default {
             });
 
             //从菜单中删除分类
-            const parent = node.parent;
-            const childrens = parent.data.subCategorys || parent.data;
-            let index = childrens.findIndex((item) => item.catId == data.catId);
-            childrens.splice(index, 1);
-            return;
+            // const parent = node.parent;
+            // const childrens = parent.data.subCategorys || parent.data;
+            // let index = childrens.findIndex((item) => item.catId == data.catId);
+            // childrens.splice(index, 1);
+            //刷新菜单
+            this.getMenus();
           });
         })
         .catch(() => {});
